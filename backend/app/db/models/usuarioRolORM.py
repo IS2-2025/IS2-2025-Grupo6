b45fc048b8970e04617ce14042dda9984a2a40e1
Matias Vara <154611628+matiasvara27@users.noreply.github.com>
2025-09-01 13:28:54 -0300

    Update usuarioRolORM.py
    Añadiendo clase UsuarioRol

--- backend/app/db/models/usuarioRolORM.py
+++ backend/app/db/models/usuarioRolORM.py
@@ -1,3 +1,25 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, Integer
 from app.db.base import Base
+from .usuario import Usuario
+from .rol import Rol
+
+class UsuarioRol(Base):
+    __tablename__ = "usuario_rol"
+
+    usuario_id: Mapped[int] = mapped_column(
+        ForeignKey("usuarios.id"),
+        primary_key=True
+    )
+    rol_id: Mapped[int] = mapped_column(
+        ForeignKey("roles.id"),
+        primary_key=True
+    )
+
+    usuario: Mapped[Usuario] = relationship("Usuario", back_populates="roles")
+    rol: Mapped[Rol] = relationship("Rol", back_populates="usuarios")
+
+    def __repr__(self) -> str:
+        return f"UsuarioRol(usuario_id={self.usuario_id}, rol_id={self.rol_id})"    
+    
+# Relaciones bidireccionales (relationship con back_populates y foreign_keys). Esto permite navegar fácilmente entre las clases, por ejemplo, puedes acceder al producto relacionado con un movimiento usando movimiento.producto.    
