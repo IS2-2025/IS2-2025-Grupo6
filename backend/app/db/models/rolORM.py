e8c0464b3981f56eb0af6c2f13b3f6cb255b20a8
Matias Vara <154611628+matiasvara27@users.noreply.github.com>
2025-09-01 13:26:13 -0300

    Update rolORM.py
    Añadiendo clase Rol

--- backend/app/db/models/rolORM.py
+++ backend/app/db/models/rolORM.py
@@ -1,3 +1,16 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Integer
 from app.db.base import Base
+
+class Rol(Base):
+    __tablename__ = "roles"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    nombre: Mapped[str] = mapped_column(String(100), unique=True)
+    
+    usuarios: Mapped[list["UsuarioRol"]] = relationship(back_populates="rol")
+
+    def __repr__(self) -> str:
+        return f"Rol(id={self.id}, nombre='{self.nombre}')"
+    
+    # Falta usuarios
