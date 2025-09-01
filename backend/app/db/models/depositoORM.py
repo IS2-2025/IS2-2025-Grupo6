03fa191b4b99d51c202e6948a7dcae3ee28317b7
Matias Vara <154611628+matiasvara27@users.noreply.github.com>
2025-09-01 13:23:49 -0300

    Update depositoORM.py
    Añadiendo clase deposito

--- backend/app/db/models/depositoORM.py
+++ backend/app/db/models/depositoORM.py
@@ -1,3 +1,16 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String
 from app.db.base import Base
+
+class Deposito(Base):
+    __tablename__ = "depositos"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    nombre: Mapped[str] = mapped_column(String(100), unique=True)
+    ubicacion: Mapped[str] = mapped_column(String(100), unique=True)
+    
+    def __repr__(self) -> str:
+        return f"Deposito(id={self.id}, nombre='{self.nombre}', ubicacion='{self.ubicacion}')"
+    
+    # Falta movimientos_origen
+    # Falta movimientos_destino
