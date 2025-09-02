05b6d3bd107168300ced5aec4be27cc84ab994f9
Angel G300 Saucedo <saucedog926@gmail.com>
2025-09-01 22:44:47 -0300

    preparando entorno de alembic

---
+++ backend/alembic/versions/c7e9c1de52a5_primir_commit_para_las_migraciones.py
@@ -0,0 +1,24 @@
+"""primir commit para las migraciones
+
+Revision ID: c7e9c1de52a5
+Revises: 
+Create Date: 2025-09-01 22:43:32.656242
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c7e9c1de52a5'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import DeclarativeBase
 class Base(DeclarativeBase):
     pass
 
+#importando las clases para que alembic pueda realizar las migraciones
 from .models.depositoORM import DepositoORM
 from .models.movimientoORM import MovimientoORM
 from .models.productoORM import ProductoORM
